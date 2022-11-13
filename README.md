b919806ae39069ee669789c35aeb7370e8fe524a
kodatinikhil <75851891+kodatinikhil@users.noreply.github.com>
2022-11-13 17:19:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS-Concepts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-wa7b3g)
---
+++ index1.js
@@ -0,0 +1,7 @@
+function display(){
+  var x=5;
+  var y=9;
+  var sum=x+y;
+console.log(sum);
+ document.getElementById('abc').innerHTML=sum;
+}
